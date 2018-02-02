12f3e91672ec86e22956af8d578531b0bbe13fcc
varun kumar <marepallyvarunkumar@gmail.com>
2018-02-02 14:27:57 +0530

    adding sample code for test in index.js and README file

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.getProgressBar = function() {
+  var domElement =  "<svg width='76px' height='76px' style='-webkit-transform: rotate(-90deg);transform:rotate(-90deg)'><g><circle r='30' cy='38' cx='38' stroke-width='1' stroke='black' fill='none' stroke-dasharray='189'></circle><circle r='30' cy='38' cx='38' stroke-width='10' stroke='green' fill='none' stroke-dasharray='59 189'></circle></g></svg>";
+	return domElement;
+}
